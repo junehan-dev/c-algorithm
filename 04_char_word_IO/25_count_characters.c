6a21076d504817f7279f2bb1a74c6a9320f0867e
Jihoon Han <jihhan@c6r7s8.42seoul.kr>
2020-01-26 22:48:51 +0900

    04_check_input_and_notify_it

---
+++ 04_char_word_IO/25_count_characters.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+/* 
+// long+while
+	long nc; // 4byte
+
+	nc = 0;
+
+	while (getchar() != EOF)
+		++nc;
+	printf("%ld\n", nc);
+*/
+
+	double nc;
+	for (nc = 0 ; getchar() != EOF; ++nc);
+	printf("%.0f\n", nc);
+}
