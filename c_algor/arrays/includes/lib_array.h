65fbacea3115644e1ad49791bd1fa978c4ec5c01
junehan <junehan.dev@gmail.com>
2022-04-07 00:02:42 +0900

    NEW/arr/subarraymax

--- c_algor/arrays/includes/lib_array.h
+++ c_algor/arrays/includes/lib_array.h
@@ -3,11 +3,13 @@
 #include <unistd.h>
 /* SEARCH */
 int		binarysearch(int arr[], int size, int value);
+int		subarray_max(int *arr, size_t size);
 
 /* MANIPULATION */
 void	reverse(int *arr, int from, int to);
 void	rotate(int *arr, size_t size, int n);
 #endif
+
 
 
 
